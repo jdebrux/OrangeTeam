a3d1cc65fae78fa180d72cd76161beb130365cde
Eashan Gandhi <eashan.gandhi@qa-training.net>
2023-10-20 10:51:32 +0100

    Connor update

--- UniqueSum.java
+++ UniqueSum.java
@@ -1,4 +1,20 @@
 public class UniqueSum {
 
 
+    public static int uniqueSumMethod(int num1, int num2, int num3){
+        int total = 0;
+
+        if(num1 != num2 && num1 != num3 && num2 != num3){
+            total += num1 + num2 + num3;
+        }
+        else if(num1 == num2 && num2 != num3 ){
+            total += num3;
+        } else if (num1 != num2 && num1 == num3){
+            total += num2;
+        } else if (num1 != num2 && num1 != num3){
+            total += num1;
+        }
+
+        return total;
+    }
 }
