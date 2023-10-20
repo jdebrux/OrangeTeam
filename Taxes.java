61bd99f3f21706b614e15d332d435db907a0852f
Jon Beresford <jonathan_beresford@live.co.uk>
2023-10-20 14:04:41 +0100

    made Taxes better

--- Taxes.java
+++ Taxes.java
@@ -1,5 +1,20 @@
 import java.util.Scanner;
 public class Taxes {
+    static String makePretty(double t) {
+        t = (t * 100 - ((t * 100) % 1)) / 100;
+        String taxString = "" + t;
+        int decimalFinder = taxString.indexOf(".");
+        int decimals = taxString.length() - decimalFinder;
+        if (decimals == 2) {
+            taxString = taxString + "0";
+        }
+        return taxString;
+    }
+    static void printBracket(double t, String bracket){
+        System.out.println("You are in the " + bracket + " tax bracket.\n" +
+                "You will be paying £" + makePretty(t) + " in tax. Here's the breakdown:");
+    }
+
     public static void main(String[] args) {
         Scanner inScanner = new Scanner(System.in);
         System.out.println("Input income here");
@@ -12,26 +27,35 @@ public class Taxes {
             double tax;
             if (income < 20000) {
                 tax = (income - 15000) * 0.1;
-                taxBracket = "10%";
+                printBracket(tax, "10%");
+                System.out.println("You earn the first £15000 tax free.");
+                System.out.println("This leaves £" + makePretty(income - 15000) + ", of which you pay 10%, or £" +
+                        makePretty(tax) + " in tax");
             } else if (income < 30000) {
                 tax = ((income - 20000) * 0.15) + 500;
-                taxBracket = "15%";
+                printBracket(tax, "15%");
+                System.out.println("You earn the first £15000 tax free.");
+                System.out.println("You pay 10% on your next £5000, which is £500");
+                System.out.println("This leaves £" + makePretty(income - 20000) + ", of which you pay 15%, or £" +
+                        makePretty(tax - 500) + " in tax");
             } else if (income < 45000) {
                 tax = ((income - 30000) * 0.2) + 2000;
-                taxBracket = "20%";
+                printBracket(tax, "20%");
+                System.out.println("You earn the first £15000 tax free.");
+                System.out.println("You pay 10% on your next £5000, which is £500");
+                System.out.println("You pay 15% on your next £10000, which is £1500");
+                System.out.println("This leaves £" + makePretty(income - 30000) + ", of which you pay 20%, or £" +
+                        makePretty(tax - 2000) + " in tax");
             } else {
                 tax = ((income - 45000) * 0.25) + 5000;
-                taxBracket = "25%";
+                printBracket(tax, "25%");
+                System.out.println("You earn the first £15000 tax free.");
+                System.out.println("You pay 10% on your next £5000, which is £500");
+                System.out.println("You pay 15% on your next £10000, which is £1500");
+                System.out.println("You pay 20% on your next £15000, which is £3000");
+                System.out.println("This leaves £" + makePretty(income - 45000) + ", of which you pay 25%, or £" +
+                        makePretty(tax - 5000) + " in tax");
             }
-            tax = (tax * 100 - ((tax * 100) % 1)) / 100;
-            taxString = "" + tax;
-            decimalFinder = taxString.indexOf(".");
-            decimals = taxString.length() - decimalFinder;
-            if (decimals == 2) {
-                taxString = taxString + "0";
-            }
-            System.out.println("You are in the " + taxBracket + " tax bracket.\n" +
-                    "You will be paying £" + taxString + " in tax.");
         }
     }
 }
